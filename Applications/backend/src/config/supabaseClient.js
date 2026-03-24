f4c185bf712f0d954db663ab3de83d1a05e7c503
Aditya Kumar Singh <simghaditya21@gmail.com>
2026-03-24 13:14:13 +0530

    Check new rule

--- Applications/backend/src/config/supabaseClient.js
+++ Applications/backend/src/config/supabaseClient.js
@@ -7,3 +7,5 @@ const supabaseUrl = process.env.SUPABASE_URL || 'YOUR_SUPABASE_URL_HERE';
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || 'YOUR_SUPABASE_SERVICE_ROLE_KEY_HERE';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Checking new rule
